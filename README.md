341bca3b384d4c79031e84c27d5e98eaffa0e1dc
florent.haffner <florent.haffner@protonmail.com>
2019-12-29 16:52:50 +0100

    moved from a CLI to an argument based script

--- README.md
+++ README.md
@@ -4,26 +4,26 @@ Archiving Tweets from the world leaders to Arweave's Permafeed blockchain.
 
 ## Setting up this bot
 
-You can start by creating your virtual environment : `python3 -m venv venv/`
+1. You can start by creating your virtual environment : `python3 -m venv venv/`
 
-To use a venv : `source venv/bin/activate`
+2. To use the venv : `source venv/bin/activate`
 
-Adding dependencies : `pip3 install -r requirements.txt`
+3. Adding dependencies : `pip3 install -r requirements.txt`
+
+4. You need to have Twitter developer credentials. Replace the current consumer_key and secret_key by you own ;)
 
 ## Using this bot
 
-Currently under heavy develpment.
+    Currently under heavy develpment.
 
-You need to have Twitter developer credentials. Replace the current consumer_key and secret_key by you own ;)
+You can query stuff following this syntax :
 
-Actually you can interact with this CLI by using :
+`python3 tweeter-feed.py -a {account_to_query}`
 
-    python3 tweeter-feed.py
+`python3 tweeter-feed.py -t {tweets_tag}`
 
-In futur you should be querying stuff following this syntax 
+`python3 tweeter-feed.py -l mocked_list.py`
 
-`python3 tweeter-feed.py {who: mocked_list if empty} {nbr_days} {type_of_save}`
+    Those command will query the Twitter API then storing results in your filesystem.
 
-`python3 tweeter-feed.py 7day fs`
-
-`python3 tweeter-feed.py trump 30day permafeed`
+Next step : connecting to Arweave blockhain network and store every correct *.json files
--- twitter-feed.py
+++ twitter-feed.py
@@ -1,7 +1,20 @@
 from os import makedirs, path
 from requests import get, post, codes
 from logs import logger
-from mocked_list import list_to_query
+from argparse import ArgumentParser
+
+""""
+Defining arguments parser to enable query from te command line
+"""
+parser = ArgumentParser(description="BOT used to Query Twitter's API")
+parser.add_argument('-l', '--list', required=False, 
+    help="Querying a list of defined Account.")
+parser.add_argument('-t', '--tweets', dest='tweets', required=False,
+    help="Querying specific Tweets tag")
+parser.add_argument('-a', '--account', dest='account', required=False,
+    help="Querying Tweets from a specific Account")
+args = parser.parse_args()
+
 
 class TwitterFeed():
     TWITTER_API_URL = 'https://api.twitter.com/'
@@ -25,40 +38,37 @@ class TwitterFeed():
 
         if(res.status_code == codes.ok):
             self.BEARER_TOKEN = res.json()['access_token']
-            self.ask_for_user_choice()
-
+            self.args_parse_routing()
         if(res.status_code > codes.ok):
             error_message = 'Issue during connection to Twitter, verify you credentials or the URL!'
             logger.error(error_message)
             print(error_message)
 
+    def args_parse_routing(self):
+        if(args.tweets != None):
+            getArgument = vars(args)['tweets']
+            self.get_global_query_through_tweets(getArgument)
+        if(args.list != None):
+            getArgument = vars(args)['list']
+            if(getArgument == 'mocked_list.py'):
+                from mocked_list import list_to_query
+                getArgument = list_to_query
+            self.query_api_from_mocked_list(getArgument)
+        if(args.account != None):
+            getArgument = vars(args)['account']
+            self.get_tweets_from_a_specific_user(getArgument)
 
-    def ask_for_user_choice(self):
-        print('What would you like to do?')
-        print('1 -> Mocked list of Government head')
-        print('2 -> Ask for a specific account')
-        print('3 -> Get a global query through Tweets')
-        ROAD = input()
-        if(ROAD == '1'):
-            self.query_api_from_mocked_list()
-        if(ROAD == '2'):
-            self.get_tweets_from_a_specific_user()
-        if(ROAD == '3'):
-            self.get_global_query_through_tweets()
-        return
-
-    def get_tweets_from_a_specific_user(self):
-        print('Tell me what Twitter account should I query?')
-        QUERY_PARAMETER = input()
+    def get_tweets_from_a_specific_user(self, QUERY_PARAMETER):
         res = get(
             self.TWITTER_API_URL + self.SEARCH_ON_TWEETS_URL + '(from:' + QUERY_PARAMETER + ')&src=typed_query',
             headers={ 'Authorization':'Bearer ' + self.BEARER_TOKEN}
         ) 
-        print(res.json())
+        print('Querying Tweets from ' + QUERY_PARAMETER + "'s account.")
+        self.writing_in_filesystem('ACCOUNT', QUERY_PARAMETER, res.text)
 
-    def query_api_from_mocked_list(self) -> str:
-        for item in list_to_query:
-            QUERY_PARAMETER = item ['account']
+    def query_api_from_mocked_list(self, LIST_TO_QUERY) -> str:
+        for item in LIST_TO_QUERY:
+            QUERY_PARAMETER = item['account']
             print('Querying Tweets from: ' + QUERY_PARAMETER)
             
             QUERY_URL = (self.TWITTER_API_URL + self.SEARCH_ON_TWEETS_URL + 
@@ -69,13 +79,12 @@ class TwitterFeed():
             res = get(QUERY_URL, headers={ 'Authorization':'Bearer ' + self.BEARER_TOKEN })
             self.writing_in_filesystem('USER', QUERY_PARAMETER, res.text)
 
-    def get_global_query_through_tweets(self) -> object:
-        print('Enter a keyword to query :')
-        QUERY_PARAMETER = input()
+    def get_global_query_through_tweets(self, QUERY_PARAMETER) -> object:
         res = get(
             self.TWITTER_API_URL + self.SEARCH_ON_TWEETS_URL + QUERY_PARAMETER,
             headers={ 'Authorization':'Bearer ' + self.BEARER_TOKEN}
         )
+        print('Querying Tweets based on ' + QUERY_PARAMETER + "'s tag.")
         self.writing_in_filesystem('TWEETS', QUERY_PARAMETER, res.text)
 
     def writing_in_filesystem(self, TYPE, QUERY_PARAMETER, API_RESULT):
