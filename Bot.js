ad231a79a2ec90b4bfe33c123dd9ca4d1e0a3f24
florent.haffner <florent.haffner@protonmail.com>
2019-12-31 14:17:20 +0100

    enable query throught *.json

--- Bot.js
+++ Bot.js
@@ -1,7 +1,7 @@
 // Messy part of the app lol
 var argv = require('./cli_config') // Get cli_arguments
+var fs = require('fs')
 require('dotenv').config(); // Get variables from .env
-new Bot();
 
 import fetch from 'node-fetch'
 
@@ -21,7 +21,6 @@ class Bot {
 
     constructor() {
         this.getIdentityToken();
-        this.cli_based_query_routing(argv)
     }
 
     /**
@@ -30,17 +29,17 @@ class Bot {
      * Change state to get BEARER TOKEN
      */
     getIdentityToken() {
-        fetch(TW_API_URL + 'oauth2/token', {
+        fetch(this.TW_API_URL + 'oauth2/token', {
             method: 'POST',
             headers: {
-                'Authorization': 'Basic ' + Buffer.from(CONSUMER_API_KEY + ":" + API_SECRET_KEY).toString('base64'),
+                'Authorization': 'Basic ' + Buffer.from(this.CONSUMER_API_KEY + ":" + this.API_SECRET_KEY).toString('base64'),
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: 'grant_type=client_credentials'
         })
             .then(res => res.json())
             .then(data => {
-                BEARER_TOKEN = data['access_token']
+                this.BEARER_TOKEN = data['access_token']
                 this.cli_based_query_routing(argv)
         })
     }
@@ -53,20 +52,51 @@ class Bot {
     cli_based_query_routing = (argv) => {
         if (argv['tweets'] != undefined) {
             let QUERY_PARAMETER = argv['tweets']
-            console.log(QUERY_PARAMETER)
+            this.get_global_query_through_tweet_tag(QUERY_PARAMETER)
         }
         if (argv['list'] != undefined) {
             let QUERY_PARAMETER = argv['list']
-            console.log(QUERY_PARAMETER)
+            this.read_file_then_query_each_account(QUERY_PARAMETER)
         }
         if (argv['account'] != undefined) {
             let QUERY_PARAMETER = argv['account']
-            console.log(QUERY_PARAMETER)
+            this.get_tweets_query_through_account(QUERY_PARAMETER)
         }
     }
 
-    get_global_query_through_tweet_tag() {
+    get_global_query_through_tweet_tag(QUERY_PARAMETER) {
+        fetch(this.TW_API_URL + this.TWEETS_SEARCH_ENDPOINT + QUERY_PARAMETER, {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + this.BEARER_TOKEN }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data['statuses'])
+        })
+    }
 
+    get_tweets_query_through_account(QUERY_PARAMETER) {
+        fetch(this.TW_API_URL + this.ACCOUNT_SEARCH_ENDPOINT + QUERY_PARAMETER, {
+            method: 'GET',
+            headers: { 'Authorization': 'Bearer ' + this.BEARER_TOKEN }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log('Okay')
+        })
+    }
+
+    read_file_then_query_each_account(file) {
+        let data = JSON.parse(file)
+        for(let [key, value] of Object.entries(data)) {
+            let screen_name = value['screen_name']
+            console.log('Querying tweet from ' + value['name'])
+            this.get_tweets_query_through_account(screen_name)
+        }
     }
 
 }
+
+
+// Sorry not a JS expert, please send a correct PR if it bother you =D 
+new Bot();
